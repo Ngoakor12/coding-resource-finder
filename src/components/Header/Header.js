84d929fe309ebe903c97dbc5940ef78227962282
ngoakor12 <ngoakor12@gmail.com>
2021-12-11 16:47:53 +0200

    Added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>ACN Resource Finder</h1>
+      <p>
+        A slightly easier way to find topics and projects on the ACN syllabus
+      </p>
+    </header>
+  );
+};
+
+export default Header;
