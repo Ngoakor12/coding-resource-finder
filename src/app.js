bbcd7a47dca72ed4af5f651c4f7f1a5ab5ed86a8
ngoakor12 <ngoakor12@gmail.com>
2021-12-11 16:59:38 +0200

    configure cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 2856;
 const { getResources } = require("./resources");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   getResources().then((resources) => res.json({ resources: resources }));
 });
 
-app.listen(port, () => {
-  console.log(`API running at port ${port}`);
-});
+app.listen(port);
