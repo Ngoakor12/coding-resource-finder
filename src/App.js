f5186b62c40e799a5a46b93c4448751f0980497d
ngoakor12 <ngoakor12@gmail.com>
2021-12-11 16:49:16 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    try {
+      getResources();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  async function getResources() {
+    await fetch("https://acn-resource-finder-api.herokuapp.com/", {
+      method: "GET", // The method
+      mode: "no-cors", // It can be no-cors, cors, same-origin
+    }).then((data) => setResources(data));
+  }
+
+  console.log(resources);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
