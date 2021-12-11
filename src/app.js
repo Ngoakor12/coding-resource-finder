f859d96de2a2259a20f3f4b334e76d25d86ca74c
ngoakor12 <ngoakor12@gmail.com>
2021-12-11 09:42:14 +0200

    api fully working

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { getResources } = require("./resources");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  getResources().then((resources) => res.json({ resources: resources }));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`API running at port ${port}`);
 });
