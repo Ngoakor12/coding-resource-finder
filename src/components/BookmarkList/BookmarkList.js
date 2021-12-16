0505d587610f888820dae78d6f3595e8ab981e2c
ngoakor12 <ngoakor12@gmail.com>
2021-12-16 13:52:37 +0200

    Added bookmaark component

---
+++ src/components/BookmarkList/BookmarkList.js
@@ -0,0 +1,9 @@
+const BookmarkList = () => {
+  return (
+    <div className="bookmark-list">
+      <h2 className="content-placeholder">Nothing bookmarked yet</h2>
+    </div>
+  );
+};
+
+export default BookmarkList;
