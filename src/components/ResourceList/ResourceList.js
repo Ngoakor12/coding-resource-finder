b3dc1b81d745eadf7d62868d7dc622d133fd607e
ngoakor12 <ngoakor12@gmail.com>
2021-12-16 13:51:02 +0200

    Added loading placeholder text

--- src/components/ResourceList/ResourceList.js
+++ src/components/ResourceList/ResourceList.js
@@ -1,20 +1,24 @@
 const ResourceList = (props) => {
   return (
     <div className="resource-list">
-      {props.resources.map((resource) => {
-        return (
-          <a
-            key={resource.url}
-            href={resource.url}
-            target="_blank"
-            rel="noreferrer"
-            className="resource"
-          >
-            <h3 className="resource-title">{resource.title}</h3>
-            <span className="resource-type">{resource.type}</span>
-          </a>
-        );
-      })}
+      {props.resources.length !== 0 ? (
+        props.resources.map((resource) => {
+          return (
+            <a
+              key={resource.url}
+              href={resource.url}
+              target="_blank"
+              rel="noreferrer"
+              className="resource"
+            >
+              <h3 className="resource-title">{resource.title}</h3>
+              <span className="resource-type">{resource.type}</span>
+            </a>
+          );
+        })
+      ) : (
+        <h2 className="content-placeholder">Loading resources...</h2>
+      )}
     </div>
   );
 };
