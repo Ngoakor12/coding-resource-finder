e7ea3bb794d9f3d8d1ef79f0da8d5bbfdb8ab223
ngoakor12 <ngoakor12@gmail.com>
2022-01-13 09:40:13 +0200

    Refactor: updated header copy and stying

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,39 @@
 const Header = () => {
   return (
     <header className="header">
-      <h1 className="header-title">ACN Resource Finder</h1>
-      <p className="header-description">
-        An easier way to find coding related topics and projects on the ACN syllabus
-      </p>
+      <h1 className="header-title">Coding Resource Finder</h1>
+      <div className="header-details">
+        <p className="header-description">
+          An easier way to find coding related topics and projects on the{" "}
+          <a className="simple-link" href="http://syllabus.africacode.net/">
+            ACN syllabus
+          </a>
+        </p>
+        <div className="header-links">
+          <p className="creator">
+            Created by:{" "}
+            <a
+              className="simple-link"
+              href="https://ngoako.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Ngoako
+            </a>
+          </p>
+          <p className="source-code">
+            Source code:{" "}
+            <a
+              className="simple-link"
+              href="https://github.com/Ngoakor12/acn-resource-finder"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </p>
+        </div>
+      </div>
     </header>
   );
 };
