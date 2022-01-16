2f92233db87c981e45b40c75f72c58fedec75dce
ngoakor12 <ngoakor12@gmail.com>
2022-01-16 08:57:06 +0200

    feat(SearchForm): added a clear search text button

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -24,13 +24,33 @@ const SearchForm = () => {
       <input
         type="text"
         className="search-input"
-        placeholder="search a resource..."
+        placeholder="Search a resource..."
         onInput={(e) => {
           setSearchTerm(e.target.value);
         }}
         onChange={handleSearch}
         value={searchTerm}
       />
+      <div
+        className="clear-button"
+        onClick={() => setSearchTerm("")}
+        title="clear search text"
+      >
+        <svg
+          aria-hidden="true"
+          role="img"
+          // class="iconify iconify--eva"
+          width="18"
+          height="18"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="currentColor"
+            d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29l-4.3 4.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4.29-4.3l4.29 4.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42z"
+          ></path>
+        </svg>
+      </div>
     </div>
   );
 };
