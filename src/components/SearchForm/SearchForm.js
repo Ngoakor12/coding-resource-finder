0249f6f00f536db704014e7530266dce386df050
ngoakor12 <ngoakor12@gmail.com>
2022-01-17 17:56:31 +0200

    feat: added search suggestions and refactoring

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,8 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Context } from "../../Context";
 const SearchForm = () => {
   const { resources, searchTerm, setSearchTerm, setRenderedResources } =
     useContext(Context);
+  const initialSuggestions = [
+    { text: "JavaScript", isSelected: false },
+    { text: "React", isSelected: false },
+    { text: "Python", isSelected: false },
+    { text: "Java", isSelected: false },
+    { text: "Node", isSelected: false },
+    { text: "SQL", isSelected: false },
+  ];
+  const [suggestions, setSuggestions] = useState(initialSuggestions);
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchTerm]);
+
+  function searchWithSuggestion(text) {
+    setSuggestions(
+      suggestions.map((suggestion) => {
+        if (text === suggestion.text) {
+          if (suggestion.isSelected) {
+            setSearchTerm("");
+            return { text: suggestion.text, isSelected: false };
+          } else {
+            setSearchTerm(suggestion.text.toLocaleLowerCase());
+            return { text: suggestion.text, isSelected: true };
+          }
+        } else {
+          return { text: suggestion.text, isSelected: false };
+        }
+      })
+    );
+  }
 
   function handleSearch() {
     let result = [];
@@ -17,8 +48,10 @@ const SearchForm = () => {
       setRenderedResources([]);
     } else {
       setRenderedResources(resources);
+      setSuggestions(initialSuggestions);
     }
   }
+
   return (
     <div className="search-input-wrapper">
       <input
@@ -28,7 +61,6 @@ const SearchForm = () => {
         onInput={(e) => {
           setSearchTerm(e.target.value);
         }}
-        onChange={handleSearch}
         value={searchTerm}
       />
       <div
@@ -39,7 +71,6 @@ const SearchForm = () => {
         <svg
           aria-hidden="true"
           role="img"
-          // class="iconify iconify--eva"
           width="18"
           height="18"
           preserveAspectRatio="xMidYMid meet"
@@ -50,6 +81,21 @@ const SearchForm = () => {
             d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29l-4.3 4.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4.29-4.3l4.29 4.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42z"
           ></path>
         </svg>
+      </div>
+      <div className="search-suggestions">
+        {suggestions.map((suggestion) => {
+          return (
+            <p
+              key={suggestion.text}
+              className={`search-suggestion ${
+                suggestion.isSelected ? "active" : ""
+              }`}
+              onClick={() => searchWithSuggestion(suggestion.text)}
+            >
+              {suggestion.text}
+            </p>
+          );
+        })}
       </div>
     </div>
   );
