7251828771b133b52b652489d363957d6f992efd
ngoakor12 <ngoakor12@gmail.com>
2022-03-15 17:30:03 +0200

    feat: add non-working pagination buttons

--- frontend/src/components/ResourceList/ResourceList.js
+++ frontend/src/components/ResourceList/ResourceList.js
@@ -2,7 +2,36 @@ import { useContext, useEffect } from "react";
 import { Context } from "../../Context";
 import SearchForm from "../SearchForm/SearchForm";
 
-const ResourceList = () => {
+const removeBookmarkIcon = (
+  <svg
+    aria-hidden="true"
+    role="img"
+    width="32"
+    height="32"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 16 16"
+  >
+    <g fill="currentColor">
+      <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"></path>
+    </g>
+  </svg>
+);
+const bookmarkIcon = (
+  <svg
+    aria-hidden="true"
+    role="img"
+    width="32"
+    height="32"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 16 16"
+  >
+    <g fill="currentColor">
+      <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"></path>
+    </g>
+  </svg>
+);
+
+function ResourceList() {
   const {
     addBookmark,
     bookmarks,
@@ -33,18 +62,7 @@ const ResourceList = () => {
               }}
               title="remove bookmark"
             >
-              <svg
-                aria-hidden="true"
-                role="img"
-                width="32"
-                height="32"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 16 16"
-              >
-                <g fill="currentColor">
-                  <path d="M2 2v13.5a.5.5 0 0 0 .74.439L8 13.069l5.26 2.87A.5.5 0 0 0 14 15.5V2a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2z"></path>
-                </g>
-              </svg>
+              {removeBookmarkIcon}
             </button>
           ) : (
             <button
@@ -54,18 +72,7 @@ const ResourceList = () => {
               }}
               title="add bookmark"
             >
-              <svg
-                aria-hidden="true"
-                role="img"
-                width="32"
-                height="32"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 16 16"
-              >
-                <g fill="currentColor">
-                  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"></path>
-                </g>
-              </svg>
+              {bookmarkIcon}
             </button>
           );
 
@@ -89,8 +96,10 @@ const ResourceList = () => {
       ) : (
         <h2 className="content-placeholder">Loading resources...</h2>
       )}
+      <button className="load-more-btn">Load 20 more resources</button>
+      <button className="load-more-btn">Load all resources</button>
     </div>
   );
-};
+}
 
 export default ResourceList;
