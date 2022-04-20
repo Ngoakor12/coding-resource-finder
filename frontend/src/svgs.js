4259d9bd6c3638db15390f4d1e321373ced12fbb
Ngoako Ramokgopa <ngoakor12@gmail.com>
2022-04-20 20:15:15 +0200

    refactor: moved clearSearchIcon into svgs.js

--- frontend/src/svgs.js
+++ frontend/src/svgs.js
@@ -27,4 +27,20 @@ const bookmarkIcon = (
   </svg>
 );
 
-export { bookmarkIcon, removeBookmarkIcon };
+const clearSearchIcon = (
+  <svg
+    aria-hidden="true"
+    role="img"
+    width="18"
+    height="18"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29l-4.3 4.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4.29-4.3l4.29 4.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42z"
+    ></path>
+  </svg>
+);
+
+export { bookmarkIcon, removeBookmarkIcon, clearSearchIcon };
