bb8d8711e365cf507c983fcf80fa691b774c4b87
Ngoako Ramokgopa <ngoakor12@gmail.com>
2022-04-23 15:56:14 +0200

    feat: add a resource skeleton component for initial load

---
+++ frontend/src/components/ResourceSkeleton/ResourceSkeleton.js
@@ -0,0 +1,13 @@
+function SkeletonResource() {
+  return (
+    <div className="resource-skeleton">
+      <div className="left">
+        <div className="larger"></div>
+        <div className="smaller"></div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+}
+
+export default SkeletonResource;
