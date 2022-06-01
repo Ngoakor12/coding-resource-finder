fe6e9ba24beff9d6dcb2119082609a7331807a10
Ngoakor12 <ngoakor12@gmail.com>
2022-06-01 09:03:21 +0200

    chore: hardcode db collection name

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,7 +1,8 @@
 function getCurrentCollectionName() {
-  return `resources_${
-    new Date().getFullYear() + "_" + (new Date().getMonth() + 1)
-  }`;
+  // return `resources_${
+  //   new Date().getFullYear() + "_" + (new Date().getMonth() + 1)
+  // }`;
+  return `resources_2022_5`;
 }
 
 module.exports = { getCurrentCollectionName };
