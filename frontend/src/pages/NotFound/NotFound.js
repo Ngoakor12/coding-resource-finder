11227389dd9fca5710919dbd44b9e9d8c5a4a2c2
Yajana N Rao <yajananrao@gmail.com>
2022-10-16 21:07:47 +0530

    Linting

--- frontend/src/pages/NotFound/NotFound.js
+++ frontend/src/pages/NotFound/NotFound.js
@@ -1,14 +1,16 @@
-import * as React from 'react';
-import { alertIcon } from '../../svgs';
+import * as React from "react";
+import { alertIcon } from "../../svgs";
 
 export default function NotFound() {
   return (
     <div className="centered-container">
       <div>
-      {alertIcon}
-      <h3 className="page-title">Page not found</h3>
-      <p className="page-description">Check if the address you entered is correct and try again</p>
-      <button className="go-to-home-button">Go back to homepage</button>
+        {alertIcon}
+        <h3 className="page-title">Page not found</h3>
+        <p className="page-description">
+          Check if the address you entered is correct and try again
+        </p>
+        <button className="go-to-home-button">Go back to homepage</button>
       </div>
     </div>
   );
