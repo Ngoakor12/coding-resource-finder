c86c9fc3f5f4d56fc8e6a94abf7529ea33628353
ngoakor12 <ngoakor12@gmail.com>
2023-01-29 10:53:18 +0200

    chore: refactor getCurrentCollectionName function

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,6 +1,5 @@
 function getCurrentCollectionName() {
-  return `resources_${
-    new Date().getFullYear() + "_" + (new Date().getMonth() + 1)
+  return `resources_${new Date().getFullYear()}_${new Date().getMonth() + 1}
   }`;
 }
 
