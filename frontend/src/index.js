eba1f7eaa9d2f7dfad477755dcc3df36949525e5
ngoakor12 <ngoakor12@gmail.com>
2023-02-04 12:17:15 +0200

    chore: replace render with createRoot ReactDOM method

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import { ContextProvider } from "./AppContext";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <ContextProvider>
       <Router>
