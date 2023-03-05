6c68730c702c2a1b472d862f3516845722672eb6
ngoakor12 <ngoakor12@gmail.com>
2023-03-05 10:08:42 +0200

    refactor new bookmark modal story to be a bit more reusable

--- frontend/src/components/NewBookmarkModal/NewBookmarkModal.js
+++ frontend/src/components/NewBookmarkModal/NewBookmarkModal.js
@@ -1,23 +1,28 @@
 import React from "react";
 
 export default function NewBookmarkModal() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <div className="new-bookmark-group-modal">
-      <div className="new-bookmark-group-form-header">New bookmark group</div>
-      <form className="new-bookmark-group-form">
-        <div className="new-bookmark-group-form-fields">
-          <label htmlFor="bookmark-group">Group name *</label>
-          <input type="text" id="bookmark-group" />
-        </div>
-        <div className="new-bookmark-group-form-buttons">
-          <button type="button" className="new-bookmark-group-form-cancel">
-            Cancel
-          </button>
-          <button type="submit" className="new-bookmark-group-form-submit">
-            Add group
-          </button>
-        </div>
-      </form>
+    <div className="bookmark-group-modal">
+      <div className="bookmark-group-modal-header">New bookmark group</div>
+      <div className="bookmark-group-modal-body">
+        <form className="new-bookmark-group-form" onSubmit={handleSubmit}>
+          <div className="new-bookmark-group-form-fields">
+            <label htmlFor="bookmark-group">Group name *</label>
+            <input type="text" id="bookmark-group" />
+          </div>
+          <div className="new-bookmark-group-form-buttons">
+            <button type="button" className="new-bookmark-group-form-cancel">
+              Cancel
+            </button>
+            <button type="submit" className="new-bookmark-group-form-submit">
+              Add group
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
