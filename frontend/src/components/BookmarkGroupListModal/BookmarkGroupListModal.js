71bd99a02c6bae915906c9dec0e0235469d1741d
ngoakor12 <ngoakor12@gmail.com>
2023-03-05 11:07:43 +0200

    bookmark group list modal mostly there, needs custom checkboxes

--- frontend/src/components/BookmarkGroupListModal/BookmarkGroupListModal.js
+++ frontend/src/components/BookmarkGroupListModal/BookmarkGroupListModal.js
@@ -1,27 +1,33 @@
 import React from "react";
 
-export default function NewBookmarkGroupModal() {
+export default function BookmarkGroupListModal({ bookmarkGroupList }) {
   function handleSubmit(e) {
     e.preventDefault();
   }
 
   return (
     <div className="bookmark-group-modal">
-      <div className="bookmark-group-modal-header">New bookmark group</div>
+      <div className="bookmark-group-modal-header">Bookmark groups</div>
       <div className="bookmark-group-modal-body">
         <form className="bookmark-group-modal-form" onSubmit={handleSubmit}>
           <div className="bookmark-group-modal-form-fields">
-            <div className="bookmark-group-modal-form-field">
-              <label htmlFor="bookmark-group">Group name *</label>
-              <input type="text" id="bookmark-group" />
-            </div>
+            {bookmarkGroupList.map((bookmarkGroup) => {
+              return (
+                <div className="bookmark-group-modal-form-field-inline">
+                  <input type="checkbox" id={bookmarkGroup.id} />
+                  <label htmlFor={bookmarkGroup.id}>
+                    {bookmarkGroup.title}
+                  </label>
+                </div>
+              );
+            })}
           </div>
           <div className="bookmark-group-modal-buttons">
             <button type="button" className="new-bookmark-group-form-cancel">
-              Cancel
+              Close
             </button>
             <button type="submit" className="new-bookmark-group-form-submit">
-              Add group
+              New group
             </button>
           </div>
         </form>
---
+++ frontend/src/stories/BookmarkGroupListModal.stories.js
@@ -0,0 +1,18 @@
+import React from "react";
+import BookmarkGroupListModal from "../components/BookmarkGroupListModal/BookmarkGroupListModal";
+
+export default {
+  title: "components/BookmarkGroupListModal",
+  component: BookmarkGroupListModal,
+};
+
+const Template = (args) => <BookmarkGroupListModal {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  bookmarkGroupList: [
+    { id: "1", title: "Bookmarks" },
+    { id: "2", title: "Frontend revision" },
+  ],
+};
