76cabcb2a72b3c393e4c6b90f6e2b08b777e6920
ngoakor12 <ngoakor12@gmail.com>
2023-03-19 17:18:06 +0200

    refactor modals

--- frontend/src/components/BaseModal/BaseModal.js
+++ frontend/src/components/BaseModal/BaseModal.js
@@ -2,11 +2,13 @@ import React from "react";
 
 export default function BaseModal({ heading, children }) {
   return (
-    <div className="bookmark-group-modal">
-      <div className="bookmark-group-modal-header">
-        {heading ? heading : "Heading"}
+    <div className="base-modal-container">
+      <div className="bookmark-group-modal">
+        <div className="bookmark-group-modal-header">
+          {heading ? heading : "Heading"}
+        </div>
+        <div className="bookmark-group-modal-body">{children}</div>
       </div>
-      <div className="bookmark-group-modal-body">{children}</div>
     </div>
   );
 }
--- frontend/src/components/EditBookmarkGroupModal/EditBookmarkGroupModal.js
+++ frontend/src/components/EditBookmarkGroupModal/EditBookmarkGroupModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BaseModal from "../BaseModal/BaseModal";
 
 export default function EditBookmarkGroupModal() {
   function handleSubmit(e) {
@@ -6,26 +7,23 @@ export default function EditBookmarkGroupModal() {
   }
 
   return (
-    <div className="bookmark-group-modal">
-      <div className="bookmark-group-modal-header">Edit bookmark group</div>
-      <div className="bookmark-group-modal-body">
-        <form className="bookmark-group-modal-form" onSubmit={handleSubmit}>
-          <div className="bookmark-group-modal-form-fields">
-            <div className="bookmark-group-modal-form-field">
-              <label htmlFor="bookmark-group">Group name *</label>
-              <input type="text" id="bookmark-group" />
-            </div>
+    <BaseModal heading={"Edit bookmark group"}>
+      <form className="bookmark-group-modal-form" onSubmit={handleSubmit}>
+        <div className="bookmark-group-modal-form-fields">
+          <div className="bookmark-group-modal-form-field">
+            <label htmlFor="bookmark-group">Group name *</label>
+            <input type="text" id="bookmark-group" />
           </div>
-          <div className="bookmark-group-modal-buttons">
-            <button type="button" className="new-bookmark-group-form-cancel">
-              Cancel
-            </button>
-            <button type="submit" className="new-bookmark-group-form-submit">
-              Save group
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+        <div className="bookmark-group-modal-buttons">
+          <button type="button" className="new-bookmark-group-form-cancel">
+            Cancel
+          </button>
+          <button type="submit" className="new-bookmark-group-form-submit">
+            Save group
+          </button>
+        </div>
+      </form>
+    </BaseModal>
   );
 }
