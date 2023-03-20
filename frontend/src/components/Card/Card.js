24d9a3877866de1d7c915c290c62554d08045575
ngoakor12 <ngoakor12@gmail.com>
2023-03-20 13:41:11 +0200

    remove unnecessary console.log

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Card({ children, fullWidth }) {
-  console.log(fullWidth);
   return (
     <div className={`card ${fullWidth ? "full-width" : ""}`}>{children}</div>
   );
