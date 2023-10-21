aa41e925e46e441abaf1ac03d5de6349e2973003
ngoakor12 <ngoakor12@gmail.com>
2023-10-21 11:08:01 +0200

    logging of update script

--- backend/src/update-resources-database.js
+++ backend/src/update-resources-database.js
@@ -10,21 +10,37 @@ const { getAllResources } = require("./web-scrape-resources");
  */
 
 async function updateResources(database, resources = []) {
+  console.log("In updateResources");
+  console.log("Format all resources");
   const formattedResources = resources.map((resource) => ({
     title: resource.title,
     url: resource.url,
     type: resource.type,
   }));
+  console.log(
+    `Finished formatting all resources: Number of resources: ${formattedResources.length}`
+  );
+  console.log("Run getCurrentCollectionName");
   const collectionName = getCurrentCollectionName();
+  console.log(`Finished getting collection name: ${collectionName}`);
   await database.collection(collectionName).insertMany(formattedResources);
 }
 
 let db;
 connectToDb(async (err) => {
   if (!err) {
+    console.log("Start db init");
     db = await getDb();
+    console.log(`Have access to db: ${db}`);
+
+    console.log("Run getAllResources");
     getAllResources().then((res) => {
+      console.log("Finished running getAllResources");
+      console.log(`Run updateResources. Found resources length: ${res.length}`);
       updateResources(db, res);
+      console.log("Finished running updateResources");
     });
+  } else {
+    console.log(`Error: ${err}`);
   }
 });
